8c9f314c41f29956aa98336d6cbb15a389679df4
KrisK1978 <k_kempa@yahoo.co.uk>
2022-10-25 14:37:39 +0000

    Restyled form buttons i.e. color, background, etc.

--- website/views.py
+++ website/views.py
@@ -7,6 +7,11 @@ def home():
     return render_template("home.html")
 
 
+@views.route('/portfolio')
+def portfolio():
+    return render_template("portfolio.html")
+
+
 @views.route('/contact')
 def contact():
     return render_template("contact.html")
